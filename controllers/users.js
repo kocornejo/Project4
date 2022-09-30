fc4082b24174eac534f9de1b921dea01c55351f8
kocornejo <76974033+kocornejo@users.noreply.github.com>
2022-09-30 11:10:30 -0700

    more base

--- controllers/users.js
+++ controllers/users.js
@@ -7,11 +7,13 @@ const { v4: uuidv4 } = require("uuid");
 // since we are sharing code, when you pull you don't want to have to edit the
 // the bucket name, thats why we're using an environment variable
 const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const SECRET = process.env.SECRET
 
 
 module.exports = {
   signup,
-  login
+  login,
+  // profile
 };
 
 async function signup(req, res) {
@@ -21,7 +23,7 @@ async function signup(req, res) {
   // Create the key that we will store in the s3 bucket name
   // pupstagram/ <- will upload everything to the bucket so it appears
   // like its an a folder (really its just nested keys on the bucket)
-  const key = `pupstagram/${uuidv4()}-${req.file.originalname}`;
+  const key = `cryptogram/${uuidv4()}-${req.file.originalname}`;
   const params = { Bucket: BUCKET_NAME, Key: key, Body: req.file.buffer };
 
   s3.upload(params, async function (err, data) {
